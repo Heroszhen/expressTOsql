9a1bfcd76da9b8852f7fc904c3a630cf477ed2dc
zhen <zhen.yang@hotmail.fr>
2019-08-30 15:32:48 +0200

    maj

--- server.js
+++ server.js
@@ -8,8 +8,45 @@ app.use(express.json());
 app.get("/",function(req,res,next){
 	db.query('select * from abonne',(error,result)=>{
 		if(error) throw error;
-		res.send(result);
+		let myreturn = {
+			"response":"ok",
+			"data":result
+		};
+		res.send(myreturn);
 	});
+});
+
+app.get("/abonne",function(req,res,next){
+	var query = req.query;
+	if((query.prenom === undefined && query.id_abonne === undefined) || (query.prenom !== undefined && query.id_abonne !== undefined)){
+		var myreturn = {
+			"response":"error"
+		};
+		res.send(myreturn);
+	}else{
+		if(query.prenom !== undefined){
+			db.query('select * from abonne where prenom = ?',query.prenom,(error,result)=>{
+				if(error) res.send(error);
+				var myreturn = {
+					"response":"ok",
+					"data":result
+				};
+				res.send(myreturn);
+			});
+		}
+		
+		if(query.id_abonne !== undefined){
+			db.query('select * from abonne where id_abonne = ?',query.id_abonne,(error,result)=>{
+				if(error) res.send(error);
+				var myreturn = {
+					"response":"ok",
+					"data":result
+				};
+				res.send(myreturn);
+			});
+		}
+	}
+	
 });
 
 app.listen(3000);
